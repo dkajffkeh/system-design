82048bc511bc6c73de93651de495c01277e91d3d
patrickYoo <yhy1045@naver.com>
2025-07-01 00:05:24 +0900

    version checker add

---
+++ id-generator/src/main/java/com/pattalk/idgenerator/presentation/VersionCheckerController.java
@@ -0,0 +1,16 @@
+package com.pattalk.idgenerator.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RequestMapping
+@RestController
+public class VersionCheckerController {
+
+    @GetMapping
+    public Mono<String> versionChecker() {
+        return Mono.just("1.0.0");
+    }
+}
