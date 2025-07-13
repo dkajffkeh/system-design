cbd79d8eebd703a400019ed367bf89c3db51f3ff
patrickYoo <yhy1045@naver.com>
2025-07-13 15:50:34 +0900

    push

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,6 +106,18 @@ project(":api-dog") {
     }
 }
 
+project(":cloud-gateway") {
+    dependencies {
+        implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+        implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+        implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.projectreactor:reactor-test")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+}
+
 project(":protocol") {
     dependencies {
     }
